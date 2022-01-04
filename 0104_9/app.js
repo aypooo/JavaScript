a476ad43705bd8f544eba518b882e3b624e04b2c
ryungeon <91059315+aypooo@users.noreply.github.com>
2022-01-04 19:40:55 +0900

    s

--- 0104_9/app.js
+++ 0104_9/app.js
@@ -5,8 +5,9 @@ console.log(apiData)
 // import apiData from "./api.json"
 const button = document.getElementById('price-btn')
 const rootDiv = document.getElementById('root')
-const apiDataRefined = apiData.map(buildElement)
-const sortedProduct = apiData.sort(priceSort).map(buildElement)
+const searchInput = document.getElementById('search-input')
+
+
 let click = false
 
 function buildElement(product){
@@ -25,6 +26,11 @@ function priceSort(a,b){
     return a.price-b.price
 
 }
+
+
+const apiDataRefined = apiData.map(buildElement)
+const sortedProduct = apiData.sort(priceSort).map(buildElement)
+
     if(click === false){
         console.log("0")
         apiDataRefined.forEach(displayProduct)
@@ -32,15 +38,24 @@ function priceSort(a,b){
 function lowerPrice(e){
     rootDiv.innerHTML =" "
     if(click === true){
+        console.log("false")
         sortedProduct.forEach(displayProduct)
         click = false
     }
     else{
         apiDataRefined.forEach(displayProduct)
+        console.log("ture")
         click = true
     }
+}
+
+function searchItem(input){
+    apiData.name.indexOf(input)
+
 }
 
 
 
 button.addEventListener('click',lowerPrice)
+searchInput.addEventListener("input",searchItem)
+
